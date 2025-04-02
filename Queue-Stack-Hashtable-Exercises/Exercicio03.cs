0d6483e3adf5dafef6e8aefd6c15bbbc6b0f3811
matheus <matheuswe2010@hotmail.com>
2025-04-02 19:45:38 -0300

    Exercicio04 - Pilha e nome do projeto alterado

---
+++ Queue-Stack-Hashtable-Exercises/Exercicio03.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Stack_Exercises
+{
+    // Implemente um programa que contém um objeto do tipo fila e que leia
+    //10 caracteres.Para cada caractere, se ele for uma letra maiúscula,
+    //insira-o na fila.Senão, se ele for uma letra minúscula, retire um
+    //caractere da fila e mostre o na tela.Caso contrário (o caractere não é
+    //uma letra), mostre o próximo elemento a ser removido da fila
+
+    class Exercicio03
+    {
+        public static void Executar()
+        {
+            Queue fila = new Queue();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite um caracter: ");
+                string entrada = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(entrada))
+                {
+                    Console.WriteLine("Entrada inválida. Tente novamente.");
+                    i--; // Permite que o usuário insira novamente
+                }
+                char c = entrada[0];
+
+                if (char.IsUpper(c))
+                {
+                    fila.Enqueue(c);
+                    Console.WriteLine($"Caracter adicionado");
+                } else if (char.IsLower(c))
+                {
+                    if (fila.Count > 0)
+                    {
+                        Console.WriteLine($"Caracter '{fila.Dequeue()}' removido");
+                    } else
+                    {
+                        Console.WriteLine("Fila vazia, não é possivel remover elementos");
+                        i--;
+                    }
+                } else
+                {
+                    Console.WriteLine($"Próximo caracter a ser removido: {fila.Peek()}");
+                }
+
+            }
+        }
+    }
+}
---
+++ Queue-Stack-Hashtable-Exercises/Exercicio04.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Stack_Exercises
+{
+    // Implemente um programa que contém um objeto do tipo pilha e que leia
+    //10 caracteres.Para cada caractere, se ele for uma letra maiúscula,
+    //insira-o na pilha.Senão, se ele for uma letra minúscula, retire um
+    //caractere da pilha e mostre o na tela.Caso contrário (o caractere não é
+    //uma letra), mostre o próximo elemento a ser removido da pilha
+    class Exercicio04
+    {
+        public static void Executar()
+        {
+            Stack pilha = new Stack();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite um caracter: ");
+                string entrada = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(entrada))
+                {
+                    Console.WriteLine("Entrada inválida. Tente novamente.");
+                    i--; // Permite que o usuário insira novamente
+                }
+                char c = entrada[0];
+
+                if (char.IsUpper(c))
+                {
+                    pilha.Push(c);
+                    Console.WriteLine($"Caracter adicionado");
+                }
+                else if (char.IsLower(c))
+                {
+                    if (pilha.Count > 0)
+                    {
+                        Console.WriteLine($"Caracter '{pilha.Pop()}' removido");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fila vazia, não é possivel remover elementos");
+                        i--;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Próximo caracter a ser removido: {pilha.Peek()}");
+                }
+
+            }
+        }
+    }
+}
