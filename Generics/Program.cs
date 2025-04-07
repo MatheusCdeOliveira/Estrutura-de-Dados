7a75c57a09b8b461eb847dbf7ac6ee1a267a9e62
matheus <matheuswe2010@hotmail.com>
2025-04-07 20:22:56 -0300

    Exercicio 06

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -21,10 +21,8 @@ namespace Generics
             }
             return count;
         }
-
         // 2 - Faça o método public void ApagaQueue(Queue<int> origem, int início, int final) que apaga todos os elementos no intervalo
         // determinado pelos parâmetros início e final. Por exemplo, ApagaQueue(q, 7, 10) => apaga os elementos entre as posições 7 e 10.
-
         public static void ApagaQueue(Queue<int> origem, int inicio, int final)
         {
             if (inicio > final)
@@ -59,10 +57,8 @@ namespace Generics
         {
             return new Queue(origem.ToArray());
         }
-
         //4 - Faça o método public void VaiProFundo<T> (Stack<T> origem, T elemento) que empilha o elemento passado como parâmetro
         //no fundo da Stack<T> origem, ao invés de empilhar no topo.
-
         public static void VaiProFundo<T> (Stack<T> origem, T elemento)
         {
             Stack<T> pilha = new Stack<T>();
@@ -77,7 +73,6 @@ namespace Generics
                 origem.Push(pilha.Pop());
             }
         }
-
         // 5 - Faça o método public void InverteQueue<T> (Queue<T> q) que inverte a ordem dos elementos da Queue<T> q, no próprio Queue.
         // Fique à vontade para utilizar outras estruturas que julgar necessárias. Nesse exercício, você não deve utilizar o método Reverse.
         public static void InverteQueue<T> (Queue<T> q)
@@ -93,20 +88,42 @@ namespace Generics
                 q.Enqueue(pilha.Pop());
             }
         }
+        // 6 - Faça o método public void InverteStack<T> (Stack<T>  s) que inverte a ordem dos elementos do Stack<T> s,
+        // no próprio Stack. Nesse exercício, você não deve utilizar o método Reverse.
+        public static void InverteStack<T>(Stack<T> s)
+        {
+            List<T> listaTemp = new List<T>();
+            while (s.Count > 0)
+            {
+                T item = s.Pop();
+                listaTemp.Add(item);
+            }
 
+            for (int i = 0; i < listaTemp.Count; i++)
+            {
+                s.Push(listaTemp[i]);
+            }
+        }
         static void Main(string[] args)
         {
-            // Execução exercicio 01
+            // EXECUÇÃO EXERCICIO 01
             //List<string> lista = new List<string>() { "Maçã", "Uva", "Pêra", "Abacaxi", "Melão", "Melancia", "Uva", "Maçã", "Uva", "Uva" };
             //int response = QtdeOcorrencias(lista, "Uva");
             //Console.Write($"O elemento passado, aparece {response} {(response > 1 || response == 0 ? "vezes" : "vez")}");
 
-            // Execução exercicio 02
             Queue<int> fila = new Queue<int>();
+            Stack<int> pilha = new Stack<int>();
+
             for (int i = 0; i <= 9; i++)
             {
                 fila.Enqueue(i * 10);
             }
+
+            for (int i = 0; i <= 9; i++)
+            {
+                pilha.Push(i * 10);
+            }
+            // EXECUÇÃO EXERCICIO 02
             //foreach(int n in fila)
             //{
             //    Console.Write(n + " ");
@@ -119,7 +136,7 @@ namespace Generics
             //    Console.Write(n + " ");
             //}
 
-            // Execução exercicio 03
+            // EXECUÇÃO EXERCICIO 03
             //Console.WriteLine();
             //Queue novaFila = CopiaQueue(fila);
 
@@ -128,12 +145,7 @@ namespace Generics
             //    Console.Write(n + " ");
             //}
 
-            // Execução exercicio 04
-            //Stack<int> pilha = new Stack<int>();
-            //for (int i = 0; i <= 9; i++)
-            //{
-            //    pilha.Push(i * 10);
-            //}
+            // EXECUÇÃO EXERCICIO 04
             //VaiProFundo(pilha, 500);
 
             //foreach (int n in pilha)
@@ -141,9 +153,18 @@ namespace Generics
             //    Console.Write(n + " ");
             //}
 
-            // Execução exercicio 05
-            InverteQueue(fila);
-            foreach (int n in fila)
+            // EXECUÇÃO EXERCICIO 05
+            //InverteQueue(fila);
+            //foreach (int n in fila)
+            //{
+            //    Console.Write(n + " ");
+            //}
+
+
+            // EXECUÇÃO EXERCICIO 06
+            InverteStack(pilha);
+
+            foreach (int n in pilha)
             {
                 Console.Write(n + " ");
             }
