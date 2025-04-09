35871a26a87d7a611b70d9ef13993c1ee057f2f6
matheus <matheuswe2010@hotmail.com>
2025-04-09 18:34:24 -0300

    exercicio lista linear adicionado

---
+++ Lista-Linear-e-Flexivel/ListaLinear/Lista.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Linear
+{
+
+    // Implemente uma classe lista sequencial sem usar o ArrayList
+    class Lista
+    {
+        private readonly int[] array;
+        private readonly int capacity;
+        private int count;
+
+        public int Count { get => count; }
+        public int Capacity { get => capacity; }
+
+        public Lista()
+            {
+                array = new int[8];
+                count = 0;
+                capacity = array.Length;
+            }
+
+        public Lista(int capacity)
+        {
+            this.capacity = capacity;
+            array = new int[capacity];
+            count = 0;
+        }
+
+
+        public void InserirInicio(int value)
+        {
+            if (count >= array.Length)
+            {
+                throw new InvalidOperationException("Não é possível inserir, a lista está cheia.");
+            }
+
+            for (int i = count; i > count; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[0] = value;
+            count++;
+        }
+
+
+        public void InserirFim(int value)
+        {
+            if (count >= array.Length)
+            {
+                throw new InvalidOperationException("Não é possível inserir, a lista está cheia.");
+            }
+
+            array[count] = value;
+            count++;
+        }
+
+        public void Inserir(int value, int pos)
+        {
+            if (count >= array.Length)
+            {
+                throw new InvalidOperationException("Não é possível inserir, lista está cheia");
+            } else if (pos < 0 || pos > count)
+            {
+                throw new InvalidOperationException("Não é possível inserir, indice fora de alcance");
+            }
+
+                for (int i = count; i > pos; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+
+            array[pos] = value;
+            count++;
+        }
+
+        public int RemoverInicio()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Não é possível remover, a lista está vazia.");
+            }
+            int response = array[0];
+            count--;
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            return response;
+        }
+
+        public int RemoverFim()
+        {
+            if (count == 0)
+            {
+                Console.Write("Erro, lista vazia");
+                return 0;
+            }
+
+            int response = array[count];
+            count--;
+
+            return response;
+        }
+
+        public int Remover(int pos)
+        {
+            if (count == 0 || pos >= array.Length || pos < 0)
+            {
+                Console.Write("Erro, lista vazia");
+                return 0;
+            }
+            int response = array[pos];
+            count--;
+            for (int i = pos; i < count; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            return response;
+        }
+
+        public void Mostrar()
+        {
+            Console.Write("[ ");
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.Write("]");
+        }
+
+        public bool Pesquisar(int x)
+        {
+            bool response = false;
+            for (int i = 0; i < count; i++)
+            {
+                response = (array[i] == x);
+            }
+            return response;
+        }
+    }
+
+}
---
+++ Lista-Linear-e-Flexivel/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lista_Linear;
+
+namespace Lista_Linear_e_Flexivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TAD.Exercicio4Pilha tad = new TAD.Exercicio4Pilha();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    tad.Inserir(i);
+            //}
+            //Console.WriteLine(tad.Remover());
+            //tad.Inserir(10);
+            //Console.WriteLine(tad.Remover());
+            //tad.Inserir(11);
+            //Console.WriteLine(tad.Remover());
+            //tad.Inserir(12);
+
+            //Console.WriteLine("======== Mostrar");
+            //tad.Mostrar();
+
+            Lista lista = new Lista();
+            int x1, x2, x3;
+
+            lista.InserirInicio(1);
+            lista.InserirInicio(7);
+            lista.InserirFim(2);
+            lista.InserirFim(3);
+            lista.Inserir(4, 3);
+            lista.Inserir(5, 2);
+
+            Console.Write("Apos insercoes: ");
+            lista.Mostrar();
+
+            x1 = lista.RemoverInicio();
+            x2 = lista.RemoverFim();
+            x3 = lista.Remover(2);
+
+            Console.Write("Apos remocoes (" + x1 + ", " + x2 + ", " + x3 + "):");
+            lista.Mostrar();
+        }
+    }
+}
